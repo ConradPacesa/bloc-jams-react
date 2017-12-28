c6c92560da8369a34b4c20014b4cfd36c4bd4a67
Conrad Pacesa <conradpacesa@gmail.com>
2017-12-28 16:52:59 -0500

    Added play/pause to album view

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,11 +10,39 @@ class Album extends Component {
     });
 
     this.state = {
-      album: album 
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
     };
+
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
   }
 
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
+  }
 
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) { this.setSong(song); }     
+      this.play();
+    }
+  }
 
   render() {
     return (
@@ -24,7 +52,7 @@ class Album extends Component {
           <div className="album-details">
             <h1 id="album-title">{this.state.album.title}</h1>
             <h2 className="artist">{this.state.album.artist}</h2>
-            <div id="release-info">{this.state.album.releaseInfo}</div>
+            <div id="release-info">{this.state.album.year} {this.state.album.label}</div>
           </div>
         </section>
         <table id="song-list">
@@ -34,15 +62,19 @@ class Album extends Component {
              <col id="song-duration-column" />
            </colgroup>  
            <tbody>
-             {
-               this.state.album.songs.map( (song, index) => 
-                <tr>
-                  <td className="song-item-number">{index + 1}</td>
-                  <td className="song-item-title">{song.title}</td>
-                  <td className="song-item-duration">{song.duration}</td>
+             {this.state.album.songs.map( (song, index) => 
+                <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
+                  <td className="song-actions">
+                    <button>
+                      <span className="song-number">{index+1}</span>
+                      <span className="ion-play"></span>
+                      <span className="ion-pause"></span>
+                    </button>
+                  </td>
+                  <td className="song-title">{song.title}</td>
+                  <td className="song-duration">{song.duration}</td>
                 </tr>
-              )
-             }
+              )}
            </tbody>
          </table>
       </section>
